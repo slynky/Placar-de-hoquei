a25d4dc28749e35b3ecf06700543591d8452ef0e
slynky <kaua5.vs.melo@gmail.com>
2025-06-07 16:11:26 -0300

    Criação do Launcher para build

---
+++ src/main/java/org/kauamelo/placar/placardehoquei/Launcher.java
@@ -0,0 +1,8 @@
+package org.kauamelo.placar.placardehoquei;
+
+//classe para que a build funcione corretamente
+public class Launcher {
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
